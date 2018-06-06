b268673d4f47b32181d7f93a9c39bba4ef232e61
dustinruehle <druehle@pivotal.io>
2018-06-06 10:22:03 -0400

    adding a dummy method

--- src/main/java/com/dr/session/pcf/MyController.java
+++ src/main/java/com/dr/session/pcf/MyController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -14,7 +15,7 @@ public class MyController {
     @RequestMapping("/now")
     public String add(HttpSession session) {
         String timestamp = String.valueOf(Calendar.getInstance().getTimeInMillis());
-        session.setAttribute("timestamp",timestamp);
+        session.setAttribute("timestamp", timestamp);
         return timestamp;
     }
 
@@ -22,4 +23,29 @@ public class MyController {
     public Object get(HttpSession session) {
         return session.getAttribute("timestamp");
     }
+
+
+    @RequestMapping("/jasonversion")
+    public String getJasonVersion(HttpSession session) {
+        int counter = 0;
+
+        Integer sessionCounter = (Integer) session.getAttribute("jasoncounter");
+        if (sessionCounter != null) {
+            counter = sessionCounter.intValue();
+            counter++;
+        }
+
+        Date now = new Date();
+        SimpleDateFormat month = new SimpleDateFormat("MM");
+        System.err.println("Month: " + month.format(now));
+        SimpleDateFormat year = new SimpleDateFormat("YY");
+        System.err.println("Year: " + year.format(now));
+
+        String jasonversion = "5." + year.format(now) + "." + month.format(now) + "." + counter;
+
+        session.setAttribute("jasoncounter", new Integer(counter));
+
+        return jasonversion;
+    }
+
 }
